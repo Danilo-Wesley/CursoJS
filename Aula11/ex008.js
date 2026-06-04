79de93d105780b7fe6656f49eaaf841dbc2283d8
Danilo Wesley <danilodan693@gmail.com>
2026-06-04 12:57:21 -0300

    exercícios de funções e condições no console e em site

---
+++ Aula11/ex008.js
@@ -0,0 +1,6 @@
+var velo = 78.5
+console.log(`A velocidade do seu carro é ${velo} Km/h`)
+if (velo > 60){     //condição simples (sem Else)
+    console.log(`Você ultrapassou a velocidade permitida, MULTADO!`)
+}
+console.log(`Diriga sempre usando cinto de segurança!`)
