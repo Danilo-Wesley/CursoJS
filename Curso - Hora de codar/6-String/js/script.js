1ee651d9f4b92c1306ecbd67427658c9528f7d88
Danilo Wesley <danilodan693@gmail.com>
2026-07-09 13:46:39 -0300

    strings

---
+++ Curso - Hora de codar/6-String/js/script.js
@@ -0,0 +1,23 @@
+var nome = "Danilo"
+var segundoNome = "Wesley"
+
+var nomeCompleto = nome + " " + segundoNome
+
+console.log (nome)
+console.log(typeof nome)
+
+console.log(typeof '710a')
+
+console.log(nomeCompleto)
+
+console.log(typeof '123045')
+
+var frase = "O grande dia está chegando!"
+
+console.log(frase)
+console.log(typeof frase)
+
+console.log(nome + " " + frase)
+
+document.write("<h1> Ele disse: 'Olá'<h1/>")
+document.write('<h1> Ele disse: "Olá"<h1/>')
