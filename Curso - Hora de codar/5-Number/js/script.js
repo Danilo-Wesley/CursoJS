870ce2078a27d0c385c1b4c84c31437867228a6c
Danilo Wesley <danilodan693@gmail.com>
2026-07-09 13:33:40 -0300

    Numbers

---
+++ Curso - Hora de codar/5-Number/js/script.js
@@ -0,0 +1,18 @@
+var numero = 5
+
+console.log(numero)
+console.log(typeof numero)
+
+var float = 2.55
+
+console.log(float)
+console.log(typeof float)
+
+var textoComNumero = '1024'
+
+console.log(textoComNumero)
+console.log(typeof textoComNumero)
+
+console.log(NaN)
+console.log(typeof +Infinity)
+console.log(typeof -Infinity)
