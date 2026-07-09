8d284d0e30a2dea06477cdace666432c40985bf6
Danilo Wesley <danilodan693@gmail.com>
2026-07-09 13:20:09 -0300

    Variáveis JS

---
+++ Curso - Hora de codar/4-Variaveis/js/script.js
@@ -0,0 +1,36 @@
+var teste = 1
+
+console.log(teste)
+
+teste = 'Danilo'
+
+console.log(teste)
+
+var $nome = 'sla'
+var _nome = 'sla fih'
+
+console.log($nome)
+console.log(_nome)
+
+var meuPrimeiroNome =  'Danilo' // camelCase
+
+console.log(meuPrimeiroNome)
+
+var meusegundonome = 'Wesley'
+
+console.log(meusegundonome)
+
+let testando = 1
+
+const ola = 2
+
+console.log(testando)
+console.log(ola)
+
+var meuNome
+
+console.log(meuNome) // undefined
+
+meuNome = 'Danilo'
+
+console.log(meuNome)
