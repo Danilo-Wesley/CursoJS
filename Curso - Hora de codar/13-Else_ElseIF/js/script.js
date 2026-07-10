fe0278f0610ad021377ad1b077993783038ada7c
Danilo Wesley <danilodan693@gmail.com>
2026-07-10 13:40:56 -0300

    Else e ElseIF em JS

---
+++ Curso - Hora de codar/13-Else_ElseIF/js/script.js
@@ -0,0 +1,19 @@
+var nome = "Xavier"
+
+if(nome == "Pedro"){
+    console.log("O nome dele é Pedro")
+} else if(nome == "Danilo"){
+    console.log("O nome dele é Danilo")
+} else if(nome == "Xavier"){
+    console.log("seu nome é Xavier")
+} else {
+    console.log("Ele possui outro Nome")
+}
+
+var idade = 18
+
+if(idade >= 20){
+    console.log("Ele pode entrar na Festa!")
+} else if(idade >= 18){
+    console.log("Só pode entrar na Festa com autorização")
+}
