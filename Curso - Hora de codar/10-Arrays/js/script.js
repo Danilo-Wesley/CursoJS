217e241a8448f21307fdb8bca577050102beb98f
Danilo Wesley <danilodan693@gmail.com>
2026-07-10 13:00:39 -0300

    Arrays em JS

---
+++ Curso - Hora de codar/10-Arrays/js/script.js
@@ -0,0 +1,18 @@
+var arr = [18, 'Danilo', false, {teste: 1, teste: 2}] // objetos => {}
+
+console.log(arr)
+
+var arr2 = [9,8,7,6,5,4,3,2,1]
+
+console.log(arr2)
+
+console.log(arr[1])
+console.log(arr2[0])
+
+arr[4] = 10
+
+arr[0] = 20
+
+console.log(arr)
+
+console.log(typeof arr + " " +  typeof arr2)
