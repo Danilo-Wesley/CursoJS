08d0756232111df7094f362d5c98b2fcb6738bee
Danilo Wesley <danilodan693@gmail.com>
2026-07-10 13:27:12 -0300

    Operadores de comparação em JS

---
+++ Curso - Hora de codar/12-Operadores_de_comparacao/js/script.js
@@ -0,0 +1,24 @@
+var idade =18
+var possuiCarro = 1 // 0 = falso e 1 = true
+
+if(idade >= 18){
+    console.log("O usuário pode fazer a carteira")
+}
+
+if(idade < 18){
+    console.log("O usuário não está apto para fazer a carteira")
+}
+
+if(possuiCarro){
+    console.log("o usuário já pode andar de carro")
+}
+
+var nome = 'Danilo'
+
+if(nome == "Danilo"){
+    console.log("Seu nome é Danilo")
+}
+
+if(nome != "Danilo"){
+    console.log("Seu nome não é Danilo")
+}
