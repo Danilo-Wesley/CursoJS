75f18b34de94eea1010d3a763413ceaff60a80f6
Danilo Wesley <danilodan693@gmail.com>
2026-07-11 13:33:10 -0300

    Operador AND ou && em JS

---
+++ Curso - Hora de codar/15-AND/js/script.js
@@ -0,0 +1,17 @@
+var idade = 16
+
+var nome = "João" 
+
+if(idade == 16 && nome == "João"){
+    console.log("O João pode entrar na aula de esgrima")
+} else{
+    console.log("Este não é o João")
+}
+
+if(3==3 && 3>2 && true){
+    console.log("Passou")
+}
+
+if((3==3 && 3>2) && true){ //isolar 
+    console.log("Passou")
+}
