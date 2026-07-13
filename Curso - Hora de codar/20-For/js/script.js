2fe7d1b01ede4579d57061a58604a5d17b9a103e
Danilo Wesley <danilodan693@gmail.com>
2026-07-13 12:27:27 -0300

    Est. de repetição FOR

---
+++ Curso - Hora de codar/20-For/js/script.js
@@ -0,0 +1,13 @@
+for(var i = 0; i <= 100; i+=5){
+    console.log(i)
+}
+
+var arr = [1,2,3,4,5]
+for(var j = 0; j < arr.length; j++){
+    console.log(arr[j])
+}
+console.log(arr.length)
+
+for(x = 5 ; x < 100; x *= 3){
+    console.log(x)
+}
