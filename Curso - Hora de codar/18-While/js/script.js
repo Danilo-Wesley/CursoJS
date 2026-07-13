5bd3a581e42aa55f60954b7b2e6277791b162c41
Danilo Wesley <danilodan693@gmail.com>
2026-07-13 11:57:06 -0300

    While em JS

---
+++ Curso - Hora de codar/18-While/js/script.js
@@ -0,0 +1,29 @@
+var x = 0
+
+while(x <= 5){
+    
+    console.log(`Testando repetição ${x}`)
+
+    // incremetador
+    x++
+
+}
+
+var arr = ["teste", "testando" , "a", "B"]
+var y = 0
+
+while(y <= 3 ){
+    console.log(arr[y])
+
+    y++
+}
+
+var palavra = "Danilo"
+var i = 1
+
+while(i <= palavra.length){
+
+    console.log(palavra[i-1])
+
+    i += 1
+}
