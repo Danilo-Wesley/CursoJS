3c3c7213e5f750716b6bfa19318ebe10f4e46b09
Danilo Wesley <danilodan693@gmail.com>
2026-07-15 12:29:10 -0300

    Métodos de array part2 em JS

---
+++ Curso - Hora de codar/30-Metodos_array-pt2/js/script.js
@@ -0,0 +1,22 @@
+// splice - adiciona elementos no meio de um array, [indice onde cmç , se vai escluir , oq quer adicionar]
+
+var arr = [1,2,3,4,5]
+
+arr.splice(2, 0, 999)
+
+console.log(arr)
+
+arr.splice(4, 1) // removendo sem subtituir
+console.log(arr)
+
+//indexOf - acha o index de um elemento
+
+console.log(arr.indexOf(5))
+
+// join - transforma array em uma string
+var arr2 = ["Jesus", "te", "ama", "meu" , "irmão"]
+
+console.log(arr2.join(" "))
+
+// reverse - inverter um array
+console.log(arr2.reverse())
