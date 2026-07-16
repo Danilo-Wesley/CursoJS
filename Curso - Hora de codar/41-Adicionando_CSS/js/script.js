cf02baaa7b8329158e597c6658c32fa13ea8845d
Danilo Wesley <danilodan693@gmail.com>
2026-07-16 13:48:08 -0300

    Adicionando CSS com JS

---
+++ Curso - Hora de codar/41-Adicionando_CSS/js/script.js
@@ -0,0 +1,14 @@
+// seleciona o elemento
+var title = document.querySelector("#title")
+
+// adicionando o estilo
+title.style.color = "red"
+
+// background-color (sem hifen - e C maisculo)
+title.style.backgroundColor = "blue"
+
+// selecionar elemento
+var subtitle = document.querySelector(".subtitle")
+
+// adicionar vários estilos
+subtitle.style.cssText = "color: blue; background-color: aqua; font-size: 2em; "
